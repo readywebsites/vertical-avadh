33e7a998a4e20934618daceb0a5a2bfad9fd2019
readywebsites <biz499websites@gmail.com>
2026-02-18 10:12:30 +0530

    vertical-avadh

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/V5-vertical-avadh-mobile/',   // 👈 aa tamaru exact repo naam mukvu
+  base: '/vertical-avadh/',
   plugins: [react()],
 })
