5dce169b3af33f1886a2d5680e1148cd92f6475a
readywebsites <biz499websites@gmail.com>
2026-02-19 11:11:38 +0530

    gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback, useMemo, lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter  as Router, Routes, Route } from 'react-router-dom';
 import Navbar from '../components/Navbar/Navbar';
 import Hero from '../components/Hero/Hero';
 import Loader from '../components/Loader/Loader';
